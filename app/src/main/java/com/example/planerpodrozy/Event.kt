1d46a7f6b6cf3279895c3d586e47fa29894bd812
Skrolexo <rafal.skolimowski127@gmail.com>
2024-04-01 20:32:51 +0200

    Dodanie layoutu kafelka na nazwe wydarzenia plus generowanie ich w menu głównym na podstawie danych z bazy firebase

---
+++ app/src/main/java/com/example/planerpodrozy/Event.kt
@@ -0,0 +1,6 @@
+package com.example.planerpodrozy
+
+data class Event(
+    val eventId: String,
+    val eventName: String?
+)
--- app/src/main/java/com/example/planerpodrozy/MainActivity.kt
+++ app/src/main/java/com/example/planerpodrozy/MainActivity.kt
@@ -2,14 +2,10 @@ package com.example.planerpodrozy
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.planerpodrozy.R
-import com.example.planerpodrozy.databinding.ActivityCreateEventBinding
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.planerpodrozy.databinding.ActivityMainBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -17,20 +13,23 @@ import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding;
+    private lateinit var eventRecyclerView: RecyclerView
+    private lateinit var eventAdapter: EventAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        eventRecyclerView = binding.recyclerViewEvents
+        eventAdapter = EventAdapter()
+        eventRecyclerView.adapter = eventAdapter
+        eventRecyclerView.layoutManager = LinearLayoutManager(this)
+
         val currentUser= FirebaseAuth.getInstance().currentUser
         val userId= currentUser?.uid
 
-        binding.buttonCreateEvent.setOnClickListener{
-            val intent = Intent(this, CreateEventActivity::class.java)
-            startActivity(intent)
-        }
-
+        //pobieranie z bazy wydarzeń i wyświetlanie kafelków z nazwami wydarzeń
         if(userId!=null){
             val db= Firebase.firestore
 
@@ -38,15 +37,25 @@ class MainActivity : AppCompatActivity() {
                 .whereEqualTo("userId", userId)
                 .get()
                 .addOnSuccessListener { documents->
+                    val eventsList = mutableListOf<Event>()
                 for(document in documents)
                     {
+                        val eventId = document.id
                         val nazwaWydarzenia= document.getString("nazwa_wydarzenia")
-                        Log.i("TAG", "Nazwa wydarzenia $nazwaWydarzenia")
+                        eventsList.add(Event(eventId, nazwaWydarzenia))
+                        Log.i("TAG", "Nazwa wydarzenia $nazwaWydarzenia, o id $eventId")
                     }
+                    eventAdapter.submitList(eventsList)
                 }
                 .addOnFailureListener{ e->
 
                 }
+        }
+        //tworzenie wydarzenia- przycisk
+        binding.buttonCreateEvent.setOnClickListener{
+            val intent = Intent(this, CreateEventActivity::class.java)
+            Log.i("TAG", "PRZYCISK")
+            startActivity(intent)
         }
     }
 
