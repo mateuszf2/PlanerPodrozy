2475469ca3c22d2705b9859fed6690f13ac1e841
Skrolexo <rafal.skolimowski127@gmail.com>
2024-05-16 01:21:11 +0200

    Działająca logika tworząca w bazie bilanse dla każdej pary osób po utworzeniu składki- na 99% działa, trzeba zrobić jeszcze wyświetlanie bilansów

--- app/src/main/java/com/example/planerpodrozy/AddFinanseActivity.kt
+++ app/src/main/java/com/example/planerpodrozy/AddFinanseActivity.kt
@@ -1,7 +1,9 @@
 package com.example.planerpodrozy
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.planerpodrozy.databinding.ActivityAddFinanseBinding
 import com.google.firebase.Firebase
@@ -20,6 +22,9 @@ class AddFinanseActivity: AppCompatActivity() {
         val db= Firebase.firestore
         val currentUser= FirebaseAuth.getInstance().currentUser
         val userId= currentUser?.uid
+
+
+
 
         binding.buttonBack.setOnClickListener{
             intent= Intent(this, FinanseActivity::class.java)
@@ -40,7 +45,108 @@ class AddFinanseActivity: AppCompatActivity() {
                     )
                     db.collection("finanse")
                         .add(finanseData)
-                        .addOnSuccessListener { document ->
+                        .addOnSuccessListener {
+                            db.collection("wydarzenia")
+                                .document(eventId!!)
+                                .get()
+                                .addOnSuccessListener { document->
+                                    var usersNumber= document?.data?.get("usersNumber").toString().toInt()
+
+                                    val dividedAmount= amountFinanse.toFloat()/usersNumber //podzielona kwota stworzonej składki przez ilość członków, po ile każdy się składa
+
+                                    db.collection("wydarzeniaUzytkownicy")
+                                        .whereEqualTo("eventId", eventId)
+                                        .get()
+                                        .addOnSuccessListener { usersDocs ->
+                                            for(userDoc in usersDocs){
+                                                val docUserId= userDoc.getString("userId") //user aktualnie przetwarzanego członka wydarzenia
+                                                if(docUserId!=userId){ //nie potrzebny nam nasz user bo nie jesteśmy sami sobie winni pieniędzy
+                                                    db.collection("bilans") // tworzenie tabel bilansu dla każdej pary użytkowników
+                                                        .whereEqualTo("userId",userId)
+                                                        .whereEqualTo("friendId", docUserId)
+                                                        .get()
+                                                        .addOnSuccessListener { bilansDocs ->
+                                                            Log.i(TAG, "INFOSTO")
+                                                            if(bilansDocs.isEmpty){
+                                                                val newBilansData= hashMapOf(
+                                                                    "userId" to userId,
+                                                                    "friendId" to docUserId,
+                                                                    "totalBilans" to dividedAmount
+                                                                )
+                                                                db.collection("bilans")
+                                                                    .add(newBilansData)
+                                                                    .addOnSuccessListener { documents->
+
+                                                                    }
+                                                                    .addOnFailureListener { e->
+
+                                                                    }
+                                                            }
+                                                            else{
+                                                                for(bilansDoc in bilansDocs){ //mimo, że wiem że powinien znaleźć się tylko jeden dokument to i tak trzeba użyć fora, nawet jeśli będzie to tylko jedno wykonanie pętli
+                                                                    var currentBilans= bilansDoc.getDouble("totalBilans") ?: 0.0
+                                                                    currentBilans+=dividedAmount
+                                                                    val newBilansData= hashMapOf<String, Any>(
+                                                                        "totalBilans" to currentBilans
+                                                                    )
+                                                                    db.collection("bilans").document(bilansDoc.id).update(newBilansData)
+                                                                        .addOnSuccessListener { documents->
+                                                                            //Aktualizacja zakończona sukcesem
+                                                                        }
+                                                                        .addOnFailureListener { e->
+                                                                            //Błąd podczas aktualizacji dokumentu- error
+                                                                        }
+                                                                }
+                                                            }
+                                                            db.collection("bilans")
+                                                                .whereEqualTo("userId", docUserId)
+                                                                .whereEqualTo("friendId", userId)
+                                                                .get()
+                                                                .addOnSuccessListener { bilansDocs ->
+                                                                    if(bilansDocs.isEmpty){
+                                                                        val newBilansData= hashMapOf(
+                                                                            "userId" to docUserId,
+                                                                            "friendId" to userId,
+                                                                            "totalBilans" to -dividedAmount
+                                                                        )
+                                                                        db.collection("bilans")
+                                                                            .add(newBilansData)
+                                                                            .addOnSuccessListener { documents->
+
+                                                                            }
+                                                                            .addOnFailureListener { e->
+
+                                                                            }
+                                                                    }
+                                                                    else{
+                                                                        for(bilansDoc in bilansDocs){ //mimo, że wiem że powinien znaleźć się tylko jeden dokument to i tak trzeba użyć fora, nawet jeśli będzie to tylko jedno wykonanie pętli
+                                                                            var currentBilans= bilansDoc.getDouble("totalBilans") ?: 0.0
+                                                                            currentBilans-=dividedAmount
+                                                                            val newBilansData= hashMapOf<String, Any>(
+                                                                                "totalBilans" to currentBilans
+                                                                            )
+                                                                            db.collection("bilans").document(bilansDoc.id).update(newBilansData)
+                                                                        }
+                                                                    }
+                                                                }
+                                                                .addOnFailureListener { e->
+
+                                                                }
+                                                        }
+                                                        .addOnFailureListener { e->
+
+                                                        }
+
+                                                }
+                                            }
+                                        }
+                                        .addOnFailureListener { e->
+
+                                        }
+                                }
+                                .addOnFailureListener { e->
+
+                                }
 
                         }
                         .addOnFailureListener { e ->
